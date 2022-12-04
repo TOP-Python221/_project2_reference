1288c01e3b334ecca918f7a3cbd70fbe4d621d60
Gennadiy <renardes@gmail.com>
2022-12-04 15:20:53 +0500

    класс Ranges, перечислитель Matureness, файл с данными параметрической модели

--- utils/constants.py
+++ utils/constants.py
@@ -1,20 +1,43 @@
 from enum import Enum
-from typing import Callable
 from pathlib import Path
+from collections.abc import Sequence, Callable
+from typing import Annotated, TypedDict
 
 
 class Kind(Enum):
-    CAT = 'кошка'
-    DOG = 'собака'
-    FOX = 'лиса'
-    BEAR = 'медведь'
-    SNAKE = 'змея'
-    LIZARD = 'ящерица'
-    TURTLE = 'черепаха'
+    CAT = 'cat'
+    DOG = 'dog'
+    FOX = 'fox'
+    BEAR = 'bear'
+    SNAKE = 'snake'
+    LIZARD = 'lizard'
+    TURTLE = 'turtle'
     # ...
 
 
-pathlike = str | Path
-KindActions = dict[Kind, tuple[Callable]]
+class Matureness(str, Enum):
+    EGG = 'egg'
+    CUB = 'cub'
+    YOUNG = 'young'
+    ADULT = 'adult'
+    ELDER = 'elder'
 
+    @classmethod
+    def mammals(cls):
+        res = list(cls)
+        res.remove(cls.EGG)
+        return res
+
+
+pathlike = str | Path
+
+KindActions = dict[Kind, Sequence[Callable]]
+ParamRanges = Sequence[Annotated[Sequence[int], 2]]
+RangesDict = TypedDict('RangesDict', {
+    'health': ParamRanges,
+    'stamina': ParamRanges,
+    'hunger': ParamRanges,
+    'thirst': ParamRanges
+})
+MatureDays = Annotated[Sequence[int], 3] | Annotated[Sequence[int], 4]
 
