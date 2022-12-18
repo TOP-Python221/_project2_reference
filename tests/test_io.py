abfed91d20c1fcd5946ab3bb9e383494d4776f6d
Gennadiy <renardes@gmail.com>
2022-12-18 17:10:43 +0500

    тесты: ключи данных активного питомца

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,4 +1,4 @@
-from pytest import mark
+from pytest import mark, fixture
 
 from src.model.files_io import PersistenceManager as PM
 from src.utils.constants import DATA_DIR
@@ -6,6 +6,24 @@ from src.utils.constants import DATA_DIR
 
 def active_empty_files():
     return (DATA_DIR / 'tests').rglob('**/active_empty*.json')
+
+def active_full_files():
+    return (DATA_DIR / 'tests').rglob('**/active_full*.json')
+
+
+@fixture
+def active_state():
+    return 'last_state'
+
+@fixture
+def active_keys(active_state):
+    return {'kind', 'name', 'birthdate', active_state}
+
+
+@fixture
+def active_state_keys():
+    return {'timestamp', 'health', 'stamina', 'hunger', 'thirst', 'intestine', 'joy', 'activity', 'anger', 'anxiety'}
+
 
 
 class TestParametersRead:
@@ -22,6 +40,12 @@ class TestActiveRead:
     def test_empty(self, file_path):
         data = PM.read_active(file_path)
         assert data == {}, f'{file_path.name}'
+
+    @mark.parametrize('file_path', active_full_files())
+    def test_keys(self, file_path, active_keys, active_state, active_state_keys):
+        data = PM.read_active(file_path)
+        assert set(data) == active_keys, f'{file_path.name}'
+        assert set(data[active_state]) == active_state_keys, f'{file_path.name}'
 
 
 class TestActiveWrite:
